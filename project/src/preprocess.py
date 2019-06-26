31ab9c6427306b9a8c87ac27eb9a90f15a39fdf5
Sven van Hal <sven@svenvanhal.nl>
2019-06-26 13:41:49 +0200

    Pickle processed videos.

--- project/src/preprocess.py
+++ project/src/preprocess.py
@@ -5,13 +5,15 @@ from src.Video import Video
 from .VideoReader import VideoReader
 from .histograms import compute_histograms
 import os
+import pickle
 
 DATA_PATH = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data'))
 SEGMENTS_PATH = os.path.join(DATA_PATH, "segments")
 MOVIE_PATH = os.path.join(DATA_PATH, "movies")
+PICKLE_PATH = os.path.join(DATA_PATH, "pickle")
 
 
-def load_training_set(video_set):
+def load_training_set(video_set, force_renew=False):
     """
     Load and process all videos in provided training set.
     
@@ -19,30 +21,47 @@ def load_training_set(video_set):
     """
 
     for i in video_set:
+        
         # Int to movie name
         name = "{:05d}".format(i)
-        print('processing {}'.format(name), end='\r')
+        print('processing {}'.format(name), end='\r', flush=True)
         
         # Process
-        yield process_video(name)
+        yield process_video(name, force_renew)
 
 
-def process_video(name: str) -> Video:
-    video_path = os.path.join(MOVIE_PATH, name + ".mp4")
-    segments_path = os.path.join(SEGMENTS_PATH, name + ".tsv")
-
-    # Load movie in memory
-    source_video = VideoReader()
-    source_video.open(video_path)
-
-    # Load segments file
-    segment_data = np.genfromtxt(segments_path, delimiter="\t", skip_header=1, filling_values=1)
-
-    # 4. Create video object and convert segments
-    video = Video()
-    frame_iter = source_video.get_frames()
-    video.segments = np.apply_along_axis(lambda row: create_segment(frame_iter, row), arr=segment_data, axis=1)
+def process_video(name: str, force_renew=False) -> Video:
+    pickle_path = os.path.join(PICKLE_PATH, name + ".pickle")
     
+    # If processed pickle exists, load that
+    if not force_renew and os.path.isfile(pickle_path):
+    
+        # Load pickle
+        with open(pickle_path, 'rb') as f:
+            video = pickle.load(f)
+    
+    # Else process video again and store pickled
+    else:
+        
+        video_path = os.path.join(MOVIE_PATH, name + ".mp4")
+        segments_path = os.path.join(SEGMENTS_PATH, name + ".tsv")
+
+        # Load movie in memory
+        source_video = VideoReader()
+        source_video.open(video_path)
+
+        # Load segments file
+        segment_data = np.genfromtxt(segments_path, delimiter="\t", skip_header=1, filling_values=1)
+
+        # Create video object and convert segments
+        video = Video()
+        frame_iter = source_video.get_frames()
+        video.segments = np.apply_along_axis(lambda row: create_segment(frame_iter, row), arr=segment_data, axis=1)
+        
+        # Dump to pickle
+        with open(pickle_path, 'wb+') as f:
+            pickle.dump(video, f)
+        
     return video
 
 
@@ -53,21 +72,29 @@ def create_segment(video_frames, row: np.ndarray) -> Segment:
 
     # Create new segment
     s = Segment(row[1], row[3], row[0], row[2])
-
+    
     # Accumulate frames in segment
     framebuffer = []
     for _ in range(s.num_frames()): framebuffer.append(next(video_frames))
-
+        
     # Generate histograms
-    s.histograms = __generate_histograms(np.asarray(framebuffer))
+    s.histograms = generate_histograms(np.asarray(framebuffer))
 
     return s
 
 
-def __generate_histograms(framebuffer: np.ndarray) -> np.ndarray:
+def generate_histograms(framebuffer: np.ndarray) -> np.ndarray:
 #     print(framebuffer.shape)
 #     print(framebuffer)
     
-#     for first frame in buffer compute histograms
-    return compute_histograms(framebuffer[0])
+    histograms = []
     
+    for frame in framebuffer:
+        
+        if len(histograms) == 0:
+            histograms.append(compute_histograms(frame))
+        else:
+            pass
+            # Change detection
+    
+    return np.asarray(histograms)
