659d9709cfa93fe9d1e204b88dc4580ce09515cc
Sven van Hal <sven@svenvanhal.nl>
2019-06-26 15:19:17 +0200

    Improve video processing, create train/test notebook, re-pickle first 10 videos.

--- project/src/Segment.py
+++ project/src/Segment.py
@@ -10,7 +10,8 @@ class Segment:
 
     histograms: np.ndarray
 
-    def __init__(self, time_start, time_end, frame_start, frame_end) -> None:
+    def __init__(self, movie_id, time_start, time_end, frame_start, frame_end) -> None:
+        self.movie_id = movie_id
         self.time_start = time_start
         self.time_end = time_end
         self.frame_start = int(frame_start)
@@ -27,3 +28,9 @@ class Segment:
 
     def get_hist(self, loc: tuple) -> np.ndarray:
         return self.histograms[loc[0] * 3 + loc[1]]
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "<Segment[{:s}, start={:.1f} sec, end={:.1f} sec, duration={:.1f} sec, num_frames={:d}]>".format(self.movie_id, self.time_start, self.time_end, self.duration(), self.num_frames())
--- project/src/Video.py
+++ project/src/Video.py
@@ -3,6 +3,16 @@ from src.Segment import Segment
 
 class Video:
     segments: list
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
 
     def add_segment(self, s: Segment) -> None:
         self.segments.append(s)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "<Video[%s, %d segments]>" % (self.name, len(self.segments))
