averaging, bug

project/src/preprocess.py
@@ -97,26 +97,58 @@ def create_segment(movie_id: str, video_frames, row: np.ndarray, grid_size : int
     """"
     Row layout: [startframe, starttime, endframe, endtime]
     """
+    
 
     # Create new segment
     s = Segment(movie_id, row[1], row[3], row[0], row[2])
     
     s.histograms = []
     
+    step_size = 1 if skip_val < 0 else skip_val
+    
     # Generate histograms for frames in segment
-    for i in range(0, s.num_frames(), skip_val):
-        if i % skip_val == 0:
+    for i in range(0, s.num_frames()-1, step_size):
+        # Pick this frame
+        if skip_val > 0 and i % skip_val == 0:
             frame_histograms = compute_histograms(next(video_frames), grid_size=grid_size, bins=bins)
             s.histograms.append(frame_histograms)
-        else:
+        # Skip this frame if skip val doesnt match
+        elif skip_val > 0:
             next(video_frames)
+        # Averaging frames
+        elif skip_val < 0:
+            
+#             try:
+            # make pos again
+            avg_val = abs(skip_val)
         
-        # Only convert the first frame for now
-#         break;
-    
+            # Sometimes error here as it seems next(video_frames) is already end of list, why....
+            avg_hist = compute_histograms(next(video_frames), grid_size=grid_size, bins=bins)
+            hist_count = 1
+            
+            # Try to add the next hists, however might run into end of segment
+            try:
+                for i in range(1, avg_val):
+                    tmp_hist = compute_histograms(next(video_frames), grid_size=grid_size, bins=bins)
+                    np.add(avg_hist, tmp_hist)
+                    hist_count += 1
+            except StopIteration:
+                pass
+            
+            avg_hist = np.divide(avg_hist, hist_count)
+            s.histograms.append(avg_hist)
+#             except StopIteration:
+#                 pass
+                
     return s
 
+
+
 def get_test_video(name: str, grid_size : int, bins: []):
+    """
+    Get a random 20 second sample from video: name. 
+    Compute the histograms for this sample with grid_size and bins.
+    """
     video_path = os.path.join(MOVIE_PATH, name + ".mp4")
     segments_path = os.path.join(SEGMENTS_PATH, name + ".tsv")
 
@@ -150,18 +182,54 @@ def get_test_video(name: str, grid_size : int, bins: []):
     return histograms, start_frame, end_frame
 
 def get_test_video_set(max_vid, grid_size, bins, n=100, duration=20):
+    """
+    Get or create test n videos. Try to load from pickle to save time, or compute new ones.
+    """
     test_set = []
     labels = []
     
-    for i in range(n):
-        print('\rprocessing {}/{}'.format(i+1, n), end='', flush=True)
+    pickle_test_dir = os.path.join(PICKLE_PATH, 'test', str(int(grid_size)), '_'.join(str(int(b)) for b in bins))
+    # Create folder if it doenst exist
+    if not os.path.exists(pickle_test_dir):
+        os.makedirs(pickle_test_dir)
+    
+    #Try to load existing test pickles
+    filenames = os.listdir(pickle_test_dir)
+    while len(test_set) < n and len(test_set) < len(filenames):
+        # pick random file
+        filename = random.choice(filenames)
+        file_split = filename.split('.')[0].split('_')
+        
+        if int(file_split[0]) > max_vid:
+            continue
 
-        vid = random.choice(range(1, max_vid))
+        pickle_file = os.path.join(pickle_test_dir, filename)
+        if os.path.isfile(pickle_file): 
+            # Load pickle
+
+            with open(pickle_file, 'rb') as f:
+                test_set.append(pickle.load(f))
+                  
+            labels.append(('{}.mp4'.format(file_split[0]), file_split[1], file_split[2]))
+     
+    
+    # create remaining ones      
+    for i in range(n-len(test_set)):
+        print('\rprocessing {}/{}'.format(i+1, n), end='', flush=True)
+       
+        
+        vid = random.choice(range(1, max_vid+1))
         vid_name = "{:05d}".format(vid)
         hists, start_frame, end_frame = get_test_video(vid_name, grid_size, bins)
+        
         test_set.append(hists)
         labels.append(('{}.mp4'.format(vid_name), start_frame, end_frame))
     
+        # Save for future usage
+        pickle_path = os.path.join(pickle_test_dir, '_'.join([vid_name, str(start_frame), str(end_frame)]) + ".pickle")
+        with open(pickle_path, 'wb+') as f:
+            pickle.dump(hists, f)
+
 
     return test_set, labels
     